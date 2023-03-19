60334a24983bc867f7905e88786c3f5fab5c0913
오다혜 <fffvvv6@gmail.com>
2023-03-20 00:23:37 +0900

    feat: 중단점 정하기

--- src/utils/mediaQuery.ts
+++ src/utils/mediaQuery.ts
@@ -1,5 +1,8 @@
 import facepaint from 'facepaint'
 
-const breakpoints = [576, 768, 992, 1200]
+// galaxy fold = 280
+// 다른 기기의 breakpoint tailwindcss 참고
+const breakpoints = [280, 640, 768, 1024]
 
+// mobile first 로 디자인하려면 min-width 로 설정해야함
 export const mq = facepaint(breakpoints.map(bp => `@media (min-width: ${bp}px)`))
